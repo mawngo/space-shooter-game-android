a0fca65fccd99172c3a4098389760daa08001766
mawngo <sitdownrightnow.dev@gmail.com>
2025-05-06 20:16:35 +0700

    build: sign apk using gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.nio.file.Paths
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -12,13 +14,27 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
+        buildToolsVersion = "35.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "asteroids-v${versionName}")
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(Paths.get(project.rootDir.absolutePath, "keystore.jks"))
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig =
+                if (file(Paths.get(project.rootDir.absolutePath, "keystore.jks")).exists())
+                    signingConfigs.getByName("release") else null
+
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
